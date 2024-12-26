73d059a0e29be0d27b060f317fe1079600a3be60
Shree <shreesottamkumar@gmail.com>
2024-12-26 11:48:50 +0200

    dynamic memory allocation

--- basic/src/main.cpp
+++ basic/src/main.cpp
@@ -1,39 +1,27 @@
 #include <iostream>
 #include <basic.h>
+#include <stdlib.h>
+#include <stdio.h>
 
 int main() {
-    float i{343.324};
-    float *x = &i;
-    std::cout<<x<<std::endl;
-    std::cout<<*x<<std::endl;
-    std::cout<<&x<<std::endl;
-   int a{2};
-   int b{6};
-   int sum = Add(&a, &b);
-   std::cout<<"sum: "<<sum<<std::endl;
-   Swap(&a, &b);
-   a=4;
-   b=8;
-   //AddVal(&a, &b, &sum);
-   std::cout<<"sum: "<<sum<<std::endl;
-   //Factorial(&a, &sum);
-   std::cout<<"==========================================================================================="<<std::endl;
-   //std::cout<<"sum: "<<sum<<std::endl;
-   int &z =a;
-   int &y =b;
-   int &result =sum;
-    sumup(z, y, result);
-    std::cout<<"result: "<<result<<std::endl;
-    std::cout<<"sum: "<<sum<<std::endl;
-    xt(a, b);
-    std::cout<<"a: "<<a<<" b:"<<b<<std::endl;
-    std::cout<<"z: "<<z<<" y:"<<y<<std::endl;
-    Fact(a, result);
-    std::cout<<"result: "<<result<<std::endl;
-    std::cout<<"sum: "<<sum<<std::endl;
-    //char x[512];
-    //std::cin.getline(x, 64, '\n');
-    //sstd::cout<<i<<std::endl;s
-    std::cout<<"a: "<<a<<std::endl;
+    //int *p = (int*)calloc(1,sizeof(int));
+    //int *p = (int*)malloc(sizeof(int));
+    //int *p = new int;
+    int *p = new int[5];
+    for(int i=0;i<5;i++)
+    {
+        p[i] = i;
+        std::cout<<"p: "<<p[i]<<std::endl;
+        //std::cout<<"p: "<<*p<<std::endl;
+    }
+    
+    
+    
+    delete []p;
+    
+    //free(p);
+    p=NULL;
+    //std::cout<<"p: "<<p<<std::endl;
+    //std::cout<<"p: "<<*p<<std::endl;
     return 0;
 }
